fd313c1d7f94aa6c9edf24a6b185009b4cf2710d
LordMathis <matus@namesny.com>
2024-10-22 21:47:10 +0200

    Add Workspace menu

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,14 +1,9 @@
 import React from 'react';
-import { Group, Text, ActionIcon, Avatar } from '@mantine/core';
-import { IconSettings } from '@tabler/icons-react';
+import { Group, Text, Avatar } from '@mantine/core';
+import WorkspaceMenu from './WorkspaceMenu';
 import Settings from './Settings';
-import { useModalContext } from '../contexts/ModalContext';
 
 const Header = () => {
-  const { setSettingsModalVisible } = useModalContext();
-
-  const openSettings = () => setSettingsModalVisible(true);
-
   return (
     <Group justify="space-between" h={60} px="md">
       <Text fw={700} size="lg">
@@ -16,9 +11,7 @@ const Header = () => {
       </Text>
       <Group>
         <Avatar src="https://via.placeholder.com/40" radius="xl" />
-        <ActionIcon variant="subtle" onClick={openSettings} size="lg">
-          <IconSettings size={24} />
-        </ActionIcon>
+        <WorkspaceMenu />
       </Group>
       <Settings />
     </Group>
